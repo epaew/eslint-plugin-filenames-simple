3b68c9e003640625555bce306e87177b7b8b8cca
epaew <epaew.333@gmail.com>
2020-05-30 12:43:07 +0900

    Add src/configs/index.ts: rules preset

---
+++ src/configs/index.ts
@@ -0,0 +1,9 @@
+import { all } from './all';
+import { recommended, recommendedReact, recommendedVue } from './recommended';
+
+export const configs = {
+  all,
+  recommended,
+  'recommended-react': recommendedReact,
+  'recommended-vue': recommendedVue,
+};
