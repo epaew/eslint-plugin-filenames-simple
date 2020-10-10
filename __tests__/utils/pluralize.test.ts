17e2bc85fc57be39df45dd5033222efd75c3e97a
epaew <epaew.333@gmail.com>
2020-10-10 12:27:17 +0900

    Refactor utils/pluralize.ts

--- __tests__/utils/pluralize.test.ts
+++ __tests__/utils/pluralize.test.ts
@@ -1,17 +1,8 @@
-import { Rule } from 'eslint';
-import * as pluralize from '#/utils/pluralize';
+import { Dictionaries, Pluralize } from '#/utils/pluralize';
 
 describe('pluralize.correct', () => {
-  const subject = pluralize.correct;
-
-  describe('when the rule is undefined', () => {
-    const rule = undefined;
-
-    it('returns same name', () => {
-      const names = ['test', 'tests'];
-      expect(names.map(name => subject(name, rule))).toEqual(names);
-    });
-  });
+  const pluralize = new Pluralize();
+  const subject: typeof pluralize.correct = (name, rule) => pluralize.correct(name, rule);
 
   describe('when the rule is singular', () => {
     const rule = 'singular';
@@ -33,16 +24,8 @@ describe('pluralize.correct', () => {
 });
 
 describe('pluralize.isValidName', () => {
-  const subject = pluralize.isValidName;
-
-  describe('when the rule is undefined', () => {
-    const rule = undefined;
-
-    it('returns always true', () => {
-      const names = ['test', 'tests'];
-      expect(names.map(name => subject(name, rule))).toEqual([true, true]);
-    });
-  });
+  const pluralize = new Pluralize();
+  const subject: typeof pluralize.isValidName = (name, rule) => pluralize.isValidName(name, rule);
 
   describe('when the rule is singular', () => {
     const rule = 'singular';
@@ -64,36 +47,33 @@ describe('pluralize.isValidName', () => {
 });
 
 // NOTE: This test affects other tests because the instance of `pluralize` is a singleton.
-describe('pluralize.initPluralize', () => {
-  const subject = (context: Pick<Rule.RuleContext, 'settings'>) => pluralize.initPluralize(context);
+describe('new Pluralize()', () => {
+  const subject = (dictionaries?: Dictionaries) => new Pluralize(dictionaries);
 
-  describe('when dictionaries are empty', () => {
-    const context = { settings: {} };
+  describe('when the dictionaries are undefined', () => {
+    const dictionaries = undefined;
 
-    it('does not throw error', () => {
-      subject(context);
-    });
-  });
+    it('uses default dictionaries of pluralize.', () => {
+      const pluralize = subject(dictionaries);
 
-  describe('when dictionaries are present', () => {
-    const dictionaries = {
-      irregular: [['regular', 'irregular']] as [string, string][],
-      plural: [['index', 'indexes']] as [string, string][],
-      singular: [['shoes', 'shoes']] as [string, string][],
-      uncountable: ['test'],
-    };
-    const context = { settings: { 'filenames-simple': { pluralize: dictionaries } } };
-
-    it('set the dictionaries of pluralize', () => {
-      // before setDictionaries()
       expect(pluralize.correct('regular', 'plural')).toBe('regulars');
       expect(pluralize.correct('index', 'plural')).toBe('indices');
       expect(pluralize.correct('shoes', 'singular')).toBe('shoe');
       expect(pluralize.correct('test', 'plural')).toBe('tests');
+    });
+  });
 
-      subject(context);
+  describe('when the dictionaries are present', () => {
+    const dictionaries: Dictionaries = {
+      irregular: [['regular', 'irregular']],
+      plural: [['index', 'indexes']],
+      singular: [['shoes', 'shoes']],
+      uncountable: ['test'],
+    };
 
-      // after setDictionaries()
+    it('uses customized dictionaries.', () => {
+      const pluralize = subject(dictionaries);
+
       expect(pluralize.correct('regular', 'plural')).toBe('irregular');
       expect(pluralize.correct('index', 'plural')).toBe('indexes');
       expect(pluralize.correct('shoes', 'singular')).toBe('shoes');
