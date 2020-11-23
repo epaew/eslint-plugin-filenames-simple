feb8e3b710fc8c5aa5a96318a2db22548db3f699
epaew <epaew.333@gmail.com>
2020-11-23 09:56:17 +0900

    Fixup failed test

--- __tests__/rules/index.test.ts
+++ __tests__/rules/index.test.ts
@@ -3,7 +3,8 @@ import { fetchAllRuleNames } from '../test-utils/fetch-rule-names';
 import { rules } from '#/rules';
 
 describe('Must include all rules', () => {
-  const subject = (key: string) => typeof rules[key] !== 'undefined';
+  const subject = (key: string) =>
+    (rules as { [key: string]: unknown | undefined })[key] !== undefined;
 
   test(`should include all rules`, () => {
     expect(fetchAllRuleNames().every(subject)).toBeTruthy();
