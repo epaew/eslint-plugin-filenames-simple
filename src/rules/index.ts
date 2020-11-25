27995163c566b66757f2ee9582cdfaaeb73eb29b
epaew <epaew.333@gmail.com>
2020-11-26 08:07:08 +0900

    Remove deprecated rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,4 @@
-import { casing } from './casing';
 import { extension } from './extension';
-import { extname } from './extname';
 import { namedExport } from './named-export';
 import { namingConvention } from './naming-convention';
 import { noIndex } from './no-index';
@@ -8,9 +6,7 @@ import { pluralize } from './pluralize';
 import { typescriptModuleDeclaration } from './typescript-module-declaration';
 
 export const rules = {
-  casing, // NOTE: Deprecated, I will remove this at v1.0.0
   extension,
-  extname, // NOTE: Deprecated, I will remove this at v1.0.0
   'named-export': namedExport,
   'naming-convention': namingConvention,
   'no-index': noIndex,
